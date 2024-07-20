0e63a4a7bac39ed1d85a1a47046c5d0703ec3524
PhamVanHung412004 <hp36780@gmail.com>
2024-07-20 16:49:22 +0700

    hunghung

--- content.py
+++ content.py
@@ -13,30 +13,29 @@ current_time = datetime.now()
 st.write("Thời gian hiện tại:")
 tm = current_time.strftime("%H:%M:%S")
 st.write(current_time.strftime("%H:%M:%S"))
-if (tm == '21:00:00'):
 
 # print(type(t/m))
     # # Tạo một tiêu đề cho ứng dụng
-    st.title("Các em truy cập vào link để tải bài xuống")
-    def read_word_file(file_path):
-        try:
-            doc = docx.Document(file_path)
-            content = "\n".join([paragraph.text for paragraph in doc.paragraphs])
-            return content
-        except FileNotFoundError:
-            st.error("File không tồn tại. Vui lòng kiểm tra đường dẫn.")
-            return None
-        except Exception as e:
-            st.error(f"Đã xảy ra lỗi khi đọc file: {e}")
-            return None
+st.title("Các em truy cập vào link để tải bài xuống")
+def read_word_file(file_path):
+    try:
+        doc = docx.Document(file_path)
+        content = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+        return content
+    except FileNotFoundError:
+        st.error("File không tồn tại. Vui lòng kiểm tra đường dẫn.")
+        return None
+    except Exception as e:
+        st.error(f"Đã xảy ra lỗi khi đọc file: {e}")
+        return None
 
-    # Đường dẫn tới file Word
-    file_path = "id.docx"
+# Đường dẫn tới file Word
+file_path = "id.docx"
 
-    # Đọc và hiển thị nội dung file Word
-    content = read_word_file(file_path)
-    if content:
-        st.write("Nội dung file Word:")
-        st.write(content)
-    else:
-        st.write("Không thể hiển thị nội dung file Word.")
+# Đọc và hiển thị nội dung file Word
+content = read_word_file(file_path)
+if content:
+    st.write("Nội dung file Word:")
+    st.write(content)
+else:
+    st.write("Không thể hiển thị nội dung file Word.")
