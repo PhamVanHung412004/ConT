46ead79df920ece4dbb9bb460010655074b291db
Pham Van Hung <137643495+PhamVanHung412004@users.noreply.github.com>
2024-07-20 17:43:08 +0700

    Update content.py

--- content.py
+++ content.py
@@ -3,16 +3,16 @@ import docx
 
 from datetime import datetime
 
-# Tạo một tiêu đề cho ứng dụng
-st.title("Thời gian hiện tại")
+# # Tạo một tiêu đề cho ứng dụng
+# st.title("Thời gian hiện tại")
 
-# Lấy thời gian thực
-current_time = datetime.now()
+# # Lấy thời gian thực
+# current_time = datetime.now()
 
-# Hiển thị thời gian hiện tại
-st.write("Thời gian hiện tại:")
-tm = current_time.strftime("%H:%M:%S")
-st.write(current_time.strftime("%H:%M:%S"))
+# # Hiển thị thời gian hiện tại
+# st.write("Thời gian hiện tại:")
+# tm = current_time.strftime("%H:%M:%S")
+# st.write(current_time.strftime("%H:%M:%S"))
 
 # print(type(t/m))
     # # Tạo một tiêu đề cho ứng dụng
